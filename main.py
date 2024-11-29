d7f2bdcf870e78dbc1f5860811b8c9a26858f8e7
Josue Bustamante <josuesantiagobustamanteluna@gmail.com>
2024-11-29 14:38:23 -0500

    Creacion del menu y Acerca de

---
+++ main.py
@@ -0,0 +1,37 @@
+def acerca_de():
+    """
+    Muestra información acerca del equipo que desarrolló el programa, incluyendo los integrantes,
+    roles y el eslogan del proyecto.
+    """
+    print("---------------------")
+    print("INTEGRANTES")
+    print("---------------------\n")
+    print("Josue Bustamante")
+    print("Juan David Guzman")
+    print("Jhan Albarracin")
+    print("Giovana Orejarena")
+    print("---------------------")
+    print("ROLES")
+    print("---------------------\n")
+    print("Juan David Guzman: Scrum Master")
+    print("Giovana Orejarena: Product Owner")
+    print("Josue Bustamante: Developer")
+    print("Jhan Albarracin: Developer")
+    print("---------------------")
+    print("ESLOGAN")
+    print("---------------------\n")
+    print("Más que código, creamos experiencias.\n")
+
+def mostrar_menu():
+    """
+    Muestra el menú principal con las opciones disponibles para el usuario.
+    """
+    print("<--------------------->")
+    print("GESTOR ESTUDIANTES")
+    print("<--------------------->\n")
+    print("1. Ingresar estudiante")
+    print("2. Ingresar nota")
+    print("3. Consultar estudiante")
+    print("4. Eliminar estudiante")
+    print("5. Acerca de")
+    print("6. Salir del programa\n")
